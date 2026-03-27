be269d319b9d91e342b13c9ef477a183eb450cd7
Andrei <thefurdui@gmail.com>
2026-03-27 09:29:33 +0500

    feat: initialize sterile nvim config for production environments

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Sterile Server Config. No network calls. No plugins.
+vim.opt.number = true           -- Show line numbers
+vim.opt.relativenumber = true   -- Relative line numbers for easy jumping
+vim.opt.tabstop = 4             -- 4 spaces for a tab
+vim.opt.shiftwidth = 4          -- 4 spaces for auto-indent
+vim.opt.expandtab = true        -- Convert tabs to spaces (crucial for config files)
+vim.opt.smartindent = true      -- Smart auto-indenting
+vim.opt.ignorecase = true       -- Case-insensitive searching
+vim.opt.smartcase = true        -- ...unless you type a capital letter
+vim.opt.cursorline = true       -- Highlight the current line
